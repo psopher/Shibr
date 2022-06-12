26635bb5d735358c4daf2a8deba92f2e79d2e628
Philip Sopher <phsophe@gmail.com>
2022-06-12 21:09:36 +0200

    feedback view ternary works

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -8,6 +8,8 @@ import { getImageList } from '../helpers/imageHandling'
 //mui
 import Container from '@mui/material/Container'
 import Box from '@mui/material/Box'
+import Button from '@mui/material/Button'
+import Paper from '@mui/material/Paper'
 import IconButton from '@mui/material/IconButton'
 import LinearProgress from '@mui/material/LinearProgress'
 import TextField from '@mui/material/TextField'
@@ -32,6 +34,8 @@ const Home = () => {
 
   //profile state 
   const [profiles, setProfiles] = useState([])
+  const [swiped, setSwiped] = useState(false)
+  const [isRightSwipe, setIsRightSwipe] = useState(false)
 
   const user = getUserFromLocalStorage()
   console.log('user is: ', user)
@@ -55,18 +59,26 @@ const Home = () => {
 
   const handleLeftSwipe = (e) => {
     console.log('HANDLE LEFT SWIPE RUNS')
-    
-    if (karma < 5) {
-      setKarma(karma + 1)
-    }
+
+    setSwiped(true)
   }
 
   const handleRightSwipe = (e) => {
     console.log('HANDLE RIGHT SWIPE RUNS')
+
+    setIsRightSwipe(true)
+    setSwiped(true)
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
     
     if (karma < 5) {
       setKarma(karma + 1)
     }
+
+    setSwiped(false)
+
   }
 
   return (
@@ -78,36 +90,65 @@ const Home = () => {
           </Container >
           : errors ?
             <RequestError />
-            :
-            // images
-            <>
-              <Container sx={{ pb: 4 }}>
-                <Container maxWidth='xs' sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', boxShadow: 4, mt: 2 }}>
-                  {/* Image List */}
-                  {getImageList(profiles[0], 1, 1, 1, 4, true)}
+            : swiped ? 
+              // Feedback View
+              <>
+                <Container width='sm' sx={{ display: 'flex', justifyContent: 'center' }}>
+                  <Paper elevation={6} sx={{ m: 5, py: 3, backgroundColor: 'cream' }} >
+                    <Box
+                      component='form'
+                      sx={{
+                        width: '100%',
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                      }}
+                      onSubmit={handleSubmit}
+                    >
+                      <Typography variant='h3' sx={{ pb: 2 }}>My Inulysis</Typography>
 
-                  {/* Progress Bar */}
-                  <Box sx={{ width: '100%', display: 'flex', justifyContent: 'center' }}>
-                    <LinearProgress variant="determinate" value={karma * 20} sx={{ width: 200, height: 10, border: 1, boxShadow: 4 }} />
-                  </Box>
+                      {/* Submit Button */}
+                      <Grid container textAlign='center'>
+                        <Grid item xs={12}>
+                          <Button variant="contained" type="submit"  sx={{ width: .5 }}>Submit</Button>
+                        </Grid>
+                      </Grid>
 
-
-                  {/* Buttons */}
-                  <Box sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'center', mt: 3, mb: 4 }}>
-                    <Box onClick={handleLeftSwipe} sx={{ ml: 6, mr: 6, boxShadow: 4, border: 2, borderRadius: '50%', display: 'flex', alignItems: 'center' }} >
-                      <IconButton aria-label="swipe-left" size="large" sx={{ color: 'red' }} >
-                        <CloseOutlinedIcon fontSize="large" />
-                      </IconButton>
                     </Box>
-                    <Box onClick={handleRightSwipe} sx={{ ml: 6, mr: 6, boxShadow: 4, border: 2, borderRadius: '50%', display: 'flex', alignItems: 'center' }} >
-                      <IconButton aria-label="swipe-right" size="large" sx={{ color: 'green' }} >
-                        <FavoriteOutlinedIcon fontSize="large" />
-                      </IconButton>
-                    </Box>
-                  </Box>
+                  </Paper>
                 </Container >
-              </Container>
-            </>
+              </>
+              :
+              // Swipe View
+              <>
+                <Container sx={{ pb: 4 }}>
+                  <Container maxWidth='xs' sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', boxShadow: 4, mt: 2 }}>
+                    {/* Image List */}
+                    {getImageList(profiles[0], 1, 1, 1, 4, true)}
+
+                    {/* Progress Bar */}
+                    <Box sx={{ width: '100%', display: 'flex', justifyContent: 'center' }}>
+                      <LinearProgress variant="determinate" value={karma * 20} sx={{ width: 200, height: 10, border: 1, boxShadow: 4 }} />
+                    </Box>
+
+
+                    {/* Buttons */}
+                    <Box sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'center', mt: 3, mb: 4 }}>
+                      <Box onClick={handleLeftSwipe} sx={{ ml: 6, mr: 6, boxShadow: 4, border: 2, borderRadius: '50%', display: 'flex', alignItems: 'center' }} >
+                        <IconButton aria-label="swipe-left" size="large" >
+                          <CloseOutlinedIcon fontSize="large" sx={{ color: 'secondary.main' }} />
+                        </IconButton>
+                      </Box>
+                      <Box onClick={handleRightSwipe} sx={{ ml: 6, mr: 6, boxShadow: 4, border: 2, borderRadius: '50%', display: 'flex', alignItems: 'center' }} >
+                        <IconButton aria-label="swipe-right" size="large" >
+                          <FavoriteOutlinedIcon fontSize="large" sx={{ color: 'primary.main' }} />
+                        </IconButton>
+                      </Box>
+                    </Box>
+
+                  </Container >
+                </Container>
+              </>
 
       }
     </>
